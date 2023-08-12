f02e6f312d30f5086c17ffee6f9f7c0c950a9524
ＭＵＨＩＬＡＮ <139101128+codewithMUHILAN@users.noreply.github.com>
2023-08-12 22:01:13 +0530

    Add files via upload

---
+++ TEXT/Gradient Text (12-08-23)/index.js
@@ -0,0 +1,21 @@
+let index = 0,
+    interval = 1000;
+
+const rand = (min, max) => 
+  Math.floor(Math.random() * (max - min + 1)) + min;
+
+const animate = star => {
+  star.style.setProperty("--star-left", `${rand(-10, 100)}%`);
+  star.style.setProperty("--star-top", `${rand(-40, 80)}%`);
+
+  star.style.animation = "none";
+  star.offsetHeight;
+  star.style.animation = "";
+}
+
+for (const star of document.getElementsByClassName("magic-star")) {
+  setTimeout(() => {
+    animate(star);
+    setInterval(() => animate(star), 1000);
+  }, index++ * (interval / 3))
+}
